c3781ac53f0d2228108dca7661213bf2a087d17a
Andrey Burlakov <andrburl2@yandex.ru>
2021-11-30 10:36:53 +0300

    Этап 3, иницилизировал приложение

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+const app = express();
+app.listen(process.env.PORT || 8080);
+
+app.use(requireHTTPS);
+app.use(express.static('dist/remember-the-tasks'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', { root: 'dist/remember-the-tasks' });
+});
+
+
